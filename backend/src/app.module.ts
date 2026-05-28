4b20c3314066cc9c4a8609ceb0325a22f2cbbcf0
D4vidJ4cob <161029089+D4vidJ4cob@users.noreply.github.com>
2026-05-28 15:11:21 +0200

    small addons for the setup of the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductController],
+  controllers: [AppController, ProductController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
